c8c8bd7b4609aef3830faff824475df2782bfe6d
jteitel10 <jteitel10@gmail.com>
2020-07-13 18:30:50 -0400

    GeoJson polygon layer and choropleth map added.

--- mapUno.py
+++ mapUno.py
@@ -22,9 +22,14 @@ map = folium.Map(location=[38.58, -99.09],
                  zoom_start=6, tiles='Stamen Terrain')
 
 fg = folium.FeatureGroup(name='My Map')
+
 for lt, ln, el, vnm in zip(lat, lon, elev, volnm):
     fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6,
                                      popup=vnm + ' ' + str(el) + 'm', fill_color=elev_color(el), color='grey', fill_opacity=0.7))
+
+fg.add_child(folium.GeoJson(data=open('world.json', 'r',
+                                      encoding='utf-8-sig').read(), style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
 map.add_child(fg)
 
 map.save("MapUno.html")
