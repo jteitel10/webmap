d1ddbb9323ec9f3cdb7868e6bf273eb1224cc21d
jteitel10 <jteitel10@gmail.com>
2020-07-13 13:08:36 -0400

    Edited popups to show different colors based on elevation

--- mapUno.py
+++ mapUno.py
@@ -5,14 +5,26 @@ import json
 data = pandas.read_csv('Volcanoes.txt')
 lat = list(data['LAT'])
 lon = list(data['LON'])
+elev = list(data['ELEV'])
+volnm = list(data['NAME'])
+
+
+def elev_color(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 
 map = folium.Map(location=[38.58, -99.09],
                  zoom_start=6, tiles='Stamen Terrain')
 
 fg = folium.FeatureGroup(name='My Map')
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln],
-                               popup='Hi i am a marker', icon=folium.Icon(color='red')))
+for lt, ln, el, vnm in zip(lat, lon, elev, volnm):
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6,
+                                     popup=vnm + ' ' + str(el) + 'm', fill_color=elev_color(el), color='grey', fill_opacity=0.7))
 map.add_child(fg)
 
 map.save("MapUno.html")
