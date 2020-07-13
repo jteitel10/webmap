8849a32de3f657dbbb035f9730a8a5e2ca61fc02
jteitel10 <jteitel10@gmail.com>
2020-07-13 18:39:07 -0400

    Implemented functionality to toggle on/off the different map layers.

--- mapUno.py
+++ mapUno.py
@@ -21,15 +21,20 @@ def elev_color(elevation):
 map = folium.Map(location=[38.58, -99.09],
                  zoom_start=6, tiles='Stamen Terrain')
 
-fg = folium.FeatureGroup(name='My Map')
+fgv = folium.FeatureGroup(name='Volcanoes')
 
 for lt, ln, el, vnm in zip(lat, lon, elev, volnm):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6,
-                                     popup=vnm + ' ' + str(el) + 'm', fill_color=elev_color(el), color='grey', fill_opacity=0.7))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6,
+                                      popup=vnm + ' ' + str(el) + 'm', fill_color=elev_color(el), color='grey', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=open('world.json', 'r',
-                                      encoding='utf-8-sig').read(), style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+fgp = folium.FeatureGroup(name='Population')
 
-map.add_child(fg)
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r',
+                                       encoding='utf-8-sig').read(), style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+
+map.add_child(folium.LayerControl())
 
 map.save("MapUno.html")
